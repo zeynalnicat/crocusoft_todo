40f5f81054da90c2fbbcb34d2370c8a89de5f5f1
nicat.zeynalli <nicat.zeynalli@digigo.work>
2026-03-04 12:04:31 +0400

    enhance: image to contain full size

--- app/src/main/java/com/example/crocusoft_todo/presentation/splash/SplashContent.kt
+++ app/src/main/java/com/example/crocusoft_todo/presentation/splash/SplashContent.kt
@@ -1,6 +1,7 @@
 package com.example.crocusoft_todo.presentation.splash
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -34,70 +36,83 @@ import com.example.crocusoft_todo.ui.navigation.AppRoutes
 fun SplashContent(
     navController: NavController
 ) {
+
     Scaffold(
 
     ) { innerPadding ->
 
+
         Box(
-            modifier = Modifier
-                .padding(innerPadding)
-                .padding(DsTheme.dimens.dp5)
-                .fillMaxSize()
+           modifier = Modifier.fillMaxSize()
         ) {
+            Image(
+                modifier = Modifier.fillMaxSize(),
+                painter = painterResource(Drawables.onboarding),
+                contentDescription = null,
+                contentScale = ContentScale.Crop
+
+
+            )
+
             Box(
                 modifier = Modifier
-                    .align(Alignment.TopStart)
-                    .offset(x = 0.dp, y = 0.dp)
-                    .rotate(-28f)
+                    .padding(innerPadding)
+                    .padding(DsTheme.dimens.dp5)
+                    .fillMaxSize()
             ) {
-                Image(
-                    painter = painterResource(Drawables.splash),
-                    contentDescription = null,
-                )
-            }
-
-            Box(
-                modifier = Modifier
-                    .offset(x = 0.dp, y = -150.dp)
-                    .align(Alignment.Center)
-            ){
-                Text(
-                    text = stringResource(Strings.splash_title),
-                    style = DTextStyle.t36SemiBold
-                )
-
-            }
-
-            Box(
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-            ) {
-                Column(
-                    verticalArrangement = Arrangement.spacedBy(DsTheme.dimens.dp9),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.TopStart)
+                        .fillMaxSize()
+                        .offset(x = 0.dp, y = 0.dp)
                 ) {
 
-                    Image(
-                        modifier = Modifier.size(
-                            width = DsTheme.dimens.splashWidth,
-                            height = DsTheme.dimens.splashHeight
-                        ),
-                        painter = painterResource(Drawables.splash_tags),
-                        contentDescription = null
+                }
+
+                Box(
+                    modifier = Modifier
+                        .offset(x = 0.dp, y = (-150).dp)
+                        .align(Alignment.Center)
+                ) {
+                    Text(
+                        text = stringResource(Strings.splash_title),
+                        style = DTextStyle.t36SemiBold
                     )
 
-                    AppButton(
-                        modifier = Modifier.fillMaxWidth(),
-                        text = stringResource(Strings.splash_continue)
-                    ) {
-                        navController.navigate(AppRoutes.HomeScreen.route)
-                    }
+                }
 
+                Box(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(DsTheme.dimens.dp9),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+
+                        Image(
+                            modifier = Modifier.size(
+                                width = DsTheme.dimens.splashWidth,
+                                height = DsTheme.dimens.splashHeight
+                            ),
+                            painter = painterResource(Drawables.splash_tags),
+                            contentDescription = null
+                        )
+
+                        AppButton(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = stringResource(Strings.splash_continue)
+                        ) {
+                            navController.navigate(AppRoutes.HomeScreen.route)
+                        }
+
+                    }
                 }
             }
+
+
         }
-
-
     }
+
 
 }
